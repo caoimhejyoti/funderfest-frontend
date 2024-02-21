a04e8ad477623bb51367c3fdc9ca637f4dd86288
caoimhejyoti <caoimhejyoti@gmail.com>
2024-02-21 10:06:05 +0800

    auto-login post create user working!

--- src/components/AuthProvider.jsx
+++ src/components/AuthProvider.jsx
@@ -11,18 +11,3 @@ export const AuthProvider = (props) => {
     </AuthContext.Provider>
   );
 };
-
-// export const AuthProvider = (props) => {
-//   const [auth, setAuth] = useState({
-//     token: window.localStorage.getItem("token"),
-//     user: window.localStorage.getItem("username"),
-//   });
-//   const [user, setUser] = useState({
-//     token: window.localStorage.getItem("username"),
-//   });
-//   return (
-//     <AuthContext.Provider value={({ auth, setAuth }, { user, setUser })}>
-//       {props.children}
-//     </AuthContext.Provider>
-//   );
-// };
