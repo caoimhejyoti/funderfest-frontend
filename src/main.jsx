285de9e4cff237172cfac3c8e6da4d93b9fa8f59
caoimhejyoti <caoimhejyoti@gmail.com>
2024-03-16 23:08:18 +0800

    adding update festival page

--- src/main.jsx
+++ src/main.jsx
@@ -11,6 +11,7 @@ import ContactPage from "./pages/ContactPage.jsx";
 import LoginPage from "./pages/UserPages/LoginPage.jsx";
 import CreateUserPage from "./pages/UserPages/CreateUserPage.jsx";
 import CreateFestivalPage from "./pages/FestivalPage/CreateFestivalPage.jsx";
+import UpdateFestivalPage from "./pages/FestivalPage/UpdateFestivalPage.jsx";
 
 // COMPONENTS
 import NavBar from "./components/NavBar.jsx";
@@ -27,6 +28,7 @@ const router = createBrowserRouter([
       { path: "/login", element: <LoginPage /> },
       { path: "/create-user", element: <CreateUserPage /> },
       { path: "/create-festival", element: <CreateFestivalPage /> },
+      { path: "/festival/:id/update", element: <UpdateFestivalPage /> },
       { path: "/festival/:id", element: <FestivalPage /> },
       { path: "/about", element: <AboutPage /> },
       { path: "/contact", element: <ContactPage /> },
