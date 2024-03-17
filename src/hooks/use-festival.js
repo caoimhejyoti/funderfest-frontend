61fc1010e8a49c5daa850afef5ec40e8e9f9f95f
caoimhejyoti <caoimhejyoti@gmail.com>
2024-03-17 18:37:36 +0800

    code tidy up

--- src/hooks/use-festival.js
+++ src/hooks/use-festival.js
@@ -2,14 +2,13 @@ import { useState, useEffect } from "react";
 import getFestival from "../api/get-festival";
 
 export default function useFestival(festivalId) {
-  console.log(`inside useFestival()`);
-  console.log(`festivalID: ${festivalId}`);
+  // console.log(`inside useFestival()`); //Used for DEBUGGING
+  // console.log(`festivalID: ${festivalId}`); //Used for DEBUGGING
   const [festival, setFestival] = useState();
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState();
 
   useEffect(() => {
-    console.log("Inside useEffect fnc"); //DEBUG: not being called
     getFestival(festivalId)
       .then((festival) => {
         setFestival(festival);
