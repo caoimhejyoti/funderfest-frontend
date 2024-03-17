5b5164cc91a8503ffafd99486b5e768a3c801866
caoimhejyoti <caoimhejyoti@gmail.com>
2024-03-17 18:41:28 +0800

    working to get update festival form working - SO CLOSE! 500 server error but data correct

--- src/api/put-update-festival.js
+++ src/api/put-update-festival.js
@@ -1,8 +1,11 @@
-async function putUpdateFestival(festivalDetails) {
+async function putUpdateFestival(festivalDetails, id) {
   console.log(JSON.stringify({ ...festivalDetails }));
+  console.log(JSON.stringify({ id }));
 
-  const url = `${import.meta.env.VITE_API_URL}/festivals/`;
+  const url = `${import.meta.env.VITE_API_URL}/festivals/${id}`;
+  console.log(url);
   const token = window.localStorage.getItem("token");
+  console.log(token);
 
   const updatedFestival = await fetch(url, {
     method: "PUT",
@@ -11,7 +14,16 @@ async function putUpdateFestival(festivalDetails) {
       Authorization: `Token ${token}`,
     },
     body: JSON.stringify({
-      ...festivalDetails,
+      title: `${festivalDetails.title}`,
+      description: `${festivalDetails.description}`,
+      goal: festivalDetails.goal,
+      image: `${festivalDetails.image}`,
+      is_open: festivalDetails.is_open,
+      date_created: festivalDetails.date_created,
+      owner: festivalDetails.owner,
+      tickets_available: festivalDetails.tickets_available,
+      start_date: festivalDetails.start_date,
+      end_date: festivalDetails.end_date,
     }),
   });
   console.log("updated festival: ", updatedFestival);
--- src/components/Festivals/UpdateFestivalForm.jsx
+++ src/components/Festivals/UpdateFestivalForm.jsx
@@ -1,8 +1,8 @@
 // HOOKS
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useParams } from "react-router-dom";
-
+import { useAuth } from "../../hooks/use-auth";
 // LIBRARIES
 import { PhotoIcon } from "@heroicons/react/24/solid";
 
@@ -10,35 +10,48 @@ import { PhotoIcon } from "@heroicons/react/24/solid";
 import getFestival from "../../api/get-festival";
 import putUpdateFestival from "../../api/put-update-festival";
 
-function UpdateFestivalForm(festivalID) {
+function UpdateFestivalForm(props) {
   const { id } = useParams();
+  const { auth, setAuth } = useAuth();
   const navigate = useNavigate();
-  const [originalFestivalDetails, setOrignalFestivalDetails] = useState({
+  const [festivalDetails, setFestivalDetails] = useState({
     title: "",
     description: "",
     goal: "",
-    image:
-      "https://github.com/caoimhejyoti/funderfest-frontend/blob/main/public/Funderfest_favicon.png?raw=true",
-    is_open: false,
-    date_created: new Date(),
-    owner: id,
+    image: "",
+    is_open: "",
+    date_created: "",
+    owner: auth.userID,
     tickets_available: false,
     start_date: "",
     end_date: "",
   });
-  const [updatedFestivalDetails, setUpdatedFestivalDetails] = useState({
-    title: "",
-    description: "",
-    goal: "",
-    image:
-      "https://github.com/caoimhejyoti/funderfest-frontend/blob/main/public/Funderfest_favicon.png?raw=true",
-    is_open: false,
-    date_created: new Date(),
-    owner: id,
-    tickets_available: false,
-    start_date: "",
-    end_date: "",
-  });
+
+  useEffect(() => {
+    if (id) {
+      console.log(id);
+      getFestival(id)
+        .then((festival) => {
+          setFestivalDetails({
+            title: `${festival.title}`,
+            description: `${festival.description}`,
+            goal: festival.goal,
+            image: `${festival.image}`,
+            is_open: festival.is_open,
+            date_created: festival.date_created,
+            owner: festival.owner,
+            tickets_available: festival.tickets_available,
+            start_date: festival.start_date,
+            end_date: festival.end_date,
+          });
+        })
+        .catch((error) => {
+          console.error("Failed to load festival details: ", error);
+        });
+    }
+  }, [id]);
+
+  console.log(festivalDetails);
 
   const handleChange = (event) => {
     const { id, value } = event.target;
@@ -50,157 +63,209 @@ function UpdateFestivalForm(festivalID) {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    if (
-      festivalDetails.title &&
-      festivalDetails.description &&
-      festivalDetails.goal &&
-      festivalDetails.start_date &&
-      festivalDetails.end_date
-    ) {
-      putUpdateFestival(festivalDetails).then((updatedFestival) => {
-        navigate(`/festival/${updatedFestival.id}`);
-      });
-    }
+    putUpdateFestival(festivalDetails, id).then((updatedFestival) => {
+      navigate(`/festival/${updatedFestival.id}`);
+    });
   };
 
   return (
-    <>
-      <h2>Update your event</h2>
-      <form className="container">
-        <div className="space-y-12">
-          {/* SECTION 1 - EVENT DETAILS */}
-          <div className="border-b border-gray-900/10 pb-12">
-            {/* EVENT TITLE */}
-            <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-              <div className="sm:col-span-4">
+    <main className="container">
+      {/* SECTION 1 - Current event details */}
+      <section>
+        <h2>Update your event</h2>
+        <h3>Current Event details</h3>
+        <table className="table-auto border-2 border-black">
+          <tbody>
+            <tr>
+              <td className="border-2 border-black">Current Event Name</td>
+              <td className="border-2 border-black">
+                {festivalDetails?.title}
+              </td>
+            </tr>
+            <tr>
+              <td className="border-2 border-black">
+                Current Event Description
+              </td>
+              <td className="border-2 border-black">
+                {festivalDetails?.description}
+              </td>
+            </tr>
+            <tr>
+              <td className="border-2 border-black">Current Event Goal</td>
+              <td className="border-2 border-black">
+                ${festivalDetails?.goal}
+              </td>
+            </tr>
+            <tr>
+              <td className="border-2 border-black">Current Event Image</td>
+              <td className="border-2 border-black">
+                <img src={`${festivalDetails?.image}`} alt="" />
+              </td>
+            </tr>
+            <tr>
+              <td className="border-2 border-black">Is the event published?</td>
+              <td className="border-2 border-black">
+                {festivalDetails?.is_open}
+              </td>
+            </tr>
+            <tr>
+              <td className="border-2 border-black">
+                Current Event start date
+              </td>
+              <td className="border-2 border-black">
+                {festivalDetails?.start_date}
+              </td>
+            </tr>
+            <tr>
+              <td className="border-2 border-black">Current Event end date</td>
+              <td className="border-2 border-black">
+                {festivalDetails?.end_date}
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </section>
+      {/* SECTION 2- Update form */}
+      <section>
+        <h3>Update form</h3>
+        <p>Only change the event details you need to. </p>
+        <form className="container">
+          <div className="space-y-12">
+            {/* SECTION 1 - EVENT DETAILS */}
+            <div className="border-b border-gray-900/10 pb-12">
+              {/* EVENT TITLE */}
+              <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                <div className="sm:col-span-4">
+                  <label
+                    htmlFor="title"
+                    className="after:content-['*'] after:ml-0.5 after:text-red-500"
+                  >
+                    Title:
+                  </label>
+                  <input
+                    type="text"
+                    id="title"
+                    placeholder={festivalDetails?.title}
+                    // value={festivalDetails?.title}
+                    onChange={handleChange}
+                  />
+                </div>
+              </div>
+
+              {/* EVENT DESCRIPTION */}
+              <div className="mt-10 grid grid-cols-2 gap-x-6 gap-y-8 sm:grid-cols-6">
                 <label
-                  htmlFor="title"
+                  htmlFor="description"
                   className="after:content-['*'] after:ml-0.5 after:text-red-500"
                 >
-                  Title:
+                  Description:
                 </label>
                 <input
                   type="text"
-                  id="title"
-                  placeholder="Enter Title"
+                  id="description"
+                  placeholder={festivalDetails?.description}
+                  onChange={handleChange}
+                />
+              </div>
+
+              {/* EVENT GOAL */}
+              <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                <label
+                  htmlFor="goal"
+                  className="after:content-['*'] after:ml-0.5 after:text-red-500"
+                >
+                  Goal:
+                </label>
+                <input
+                  type="int"
+                  id="goal"
+                  placeholder={festivalDetails?.goal}
+                  onChange={handleChange}
+                />
+              </div>
+
+              {/* EVENT IMAGE */}
+              <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                <label htmlFor="image">Image URL:</label>
+                <PhotoIcon
+                  className="mx-auto h-12 w-12 text-gray-300"
+                  aria-hidden="true"
+                />
+
+                <input
+                  type="text"
+                  id="image"
+                  placeholder="Provide image URL"
                   onChange={handleChange}
                 />
               </div>
             </div>
-
-            {/* EVENT DESCRIPTION */}
-            <div className="mt-10 grid grid-cols-2 gap-x-6 gap-y-8 sm:grid-cols-6">
-              <label
-                htmlFor="description"
-                className="after:content-['*'] after:ml-0.5 after:text-red-500"
-              >
-                Description:
-              </label>
-              <input
-                type="text"
-                id="description"
-                placeholder="Enter Event Description"
-                onChange={handleChange}
-              />
-            </div>
-
-            {/* EVENT GOAL */}
-            <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-              <label
-                htmlFor="goal"
-                className="after:content-['*'] after:ml-0.5 after:text-red-500"
-              >
-                Goal:
-              </label>
-              <input
-                type="int"
-                id="goal"
-                placeholder="Enter Goal value"
-                onChange={handleChange}
-              />
-            </div>
-
-            {/* EVENT IMAGE */}
-            <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-              <label htmlFor="image">Image URL:</label>
-              <PhotoIcon
-                className="mx-auto h-12 w-12 text-gray-300"
-                aria-hidden="true"
-              />
-
-              <input
-                type="text"
-                id="image"
-                placeholder="Provide image URL"
-                onChange={handleChange}
-              />
-            </div>
-          </div>
-          {/* SECTION 2 - TICKETS */}
-          <div className="border-b border-gray-900/10 pb-12">
-            <label htmlFor="tickets_available">Tickets: COMING SOON!</label>
-            {/* <input
+            {/* SECTION 2 - TICKETS */}
+            <div className="border-b border-gray-900/10 pb-12">
+              <label htmlFor="tickets_available">Tickets: COMING SOON!</label>
+              {/* <input
             type="checkbox"
             id="tickets_available"
             onChange={handleChange}
           /> */}
-          </div>
-          {/* SECTION 3 - AUTO PUBLISH? */}
-          <div className="border-b border-gray-900/10 pb-12">
-            <label
-              htmlFor="is_open"
-              className="peer-checked/draft:text-sky-950"
-            >
-              Events are automatically published. Would you like to keep this as
-              a draft?
-            </label>
-            <input
-              type="checkbox"
-              className="peer/draft"
-              id="is_open"
-              onChange={handleChange}
-            />
-            <div className="hidden peer-checked/draft:block">
-              Drafts are only visible to administrators.
             </div>
-            <div className="hidden peer-checked/published:block">
-              Your post will be publicly visible on your site.
-            </div>
-          </div>
-          {/* SECTION 4 - DATES */}
-          <div className="border-b border-gray-900/10 pb-12">
-            <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+            {/* SECTION 3 - AUTO PUBLISH? */}
+            <div className="border-b border-gray-900/10 pb-12">
               <label
-                htmlFor="start_date"
-                className="after:content-['*'] after:ml-0.5 after:text-red-500"
+                htmlFor="is_open"
+                className="peer-checked/draft:text-sky-950"
               >
-                When does your event start?
+                Events are automatically published. Would you like to keep this
+                as a draft?
               </label>
-              <input type="date" id="start_date" onChange={handleChange} />
+              <input
+                type="checkbox"
+                className="peer/draft"
+                id="is_open"
+                onChange={handleChange}
+              />
+              <div className="hidden peer-checked/draft:block">
+                Drafts are only visible to administrators.
+              </div>
+              <div className="hidden peer-checked/published:block">
+                Your post will be publicly visible on your site.
+              </div>
             </div>
-            <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-              <label
-                htmlFor="end_date"
-                className="after:content-['*'] after:ml-0.5 after:text-red-500"
+            {/* SECTION 4 - DATES */}
+            <div className="border-b border-gray-900/10 pb-12">
+              <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                <label
+                  htmlFor="start_date"
+                  className="after:content-['*'] after:ml-0.5 after:text-red-500"
+                >
+                  When does your event start?
+                </label>
+                <input type="date" id="start_date" onChange={handleChange} />
+              </div>
+              <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                <label
+                  htmlFor="end_date"
+                  className="after:content-['*'] after:ml-0.5 after:text-red-500"
+                >
+                  When does your event end?
+                </label>
+                <input type="date" id="end_date" onChange={handleChange} />
+              </div>
+            </div>
+            {/* SECTION 5 - SUBMIT */}
+            <div className="mt-6 flex items-center justify-end gap-x-6">
+              <button
+                type="submit"
+                className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                onClick={handleSubmit}
               >
-                When does your event end?
-              </label>
-              <input type="date" id="end_date" onChange={handleChange} />
+                Submit
+              </button>
             </div>
           </div>
-          {/* SECTION 5 - SUBMIT */}
-          <div className="mt-6 flex items-center justify-end gap-x-6">
-            <button
-              type="submit"
-              className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              onClick={handleSubmit}
-            >
-              Submit
-            </button>
-          </div>
-        </div>
-      </form>
-    </>
+        </form>
+      </section>
+    </main>
   );
 }
 
